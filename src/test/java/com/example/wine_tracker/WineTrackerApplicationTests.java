51d168401acfa9ba85312d0ea337bb02e3a178ec
YanNeumann <koya12355@gmail.com>
2026-03-14 17:03:31 +0300

    Added pom and other stuff

---
+++ src/test/java/com/example/wine_tracker/WineTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wine_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
